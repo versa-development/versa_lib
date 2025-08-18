5a0885b1b1f3161b2a80a70487efef8e4747de4c
Aidan <44532573+ohaidan@users.noreply.github.com>
2025-08-19 00:46:26 +0100

    choe(init): setup

---
+++ init.lua
@@ -0,0 +1,8 @@
+local config = require 'data.config'
+local log = require 'utils.logger'
+
+-- Duplicity checks if the code ran is the server or client. returns true if server
+if IsDuplicityVersion() then
+  local resourceVersion = GetResourceMetadata(GetCurrentResourceName(), 'version', 0)   -- TODO: VERSION CHECK!
+  log.info('Versa Lib v' .. resourceVersion)
+end
