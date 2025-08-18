a9d34abfa750a7e591d8a79310f77993b73d4a98
Aidan <44532573+ohaidan@users.noreply.github.com>
2025-08-19 00:44:18 +0100

    feat(utils/logger): console logging

---
+++ utils/logger.lua
@@ -0,0 +1,32 @@
+local config = require 'data.config'
+
+local Logger = {}
+
+--- Concatenates arguments into a single string, converting each argument to a string.
+local function formatLogMessage(...)
+  local args = {...}
+  for i = 1, #args do
+    args[i] = tostring(args[i])
+  end
+  return '^7' .. table.concat(args, ' ')
+end
+
+function Logger.info(...)
+  print('^5[INFO] ' .. formatLogMessage(...))
+end
+
+function Logger.warn(...)
+  print('^3[WARNING] ' .. formatLogMessage(...))
+end
+
+function Logger.error(...)
+  print('^1[ERROR] ' .. formatLogMessage(...))
+end
+
+function Logger.debug(...)
+  if config.Debug then
+    print('^2[DEBUG] ' .. formatLogMessage(...))
+  end
+end
+
+return Logger
