7b23df96c3879019c894a0ac356eff1abea4e52a
Aidan <44532573+ohaidan@users.noreply.github.com>
2025-08-20 01:20:25 +0100

    chore(progress): qbcore progress bar bridge

---
+++ bridge/progress/qb/client.lua
@@ -0,0 +1,44 @@
+local progressBar = {}
+
+function progressBar.init(data)
+  local label = data.label or ''
+  local duration = data.duration or 5000
+  local prop = data.prop or nil
+  local states = data.states or nil
+  
+  local animation = data.animation and {
+    animDict = data.animation.dict,
+    anim = data.animation.name,
+    flags = data.animation.flags,
+  } or nil
+
+  -- checking if there are multiple props defined
+  if data.prop and type(data.prop) == 'table' and next(data.prop) then
+    prop = data.prop[1]
+    prop2 = data.prop[2]
+  elseif data.prop and type(data.prop) == 'table' and not next(data.prop) then
+    prop = data.prop
+    prop2 = nil
+  end
+    
+  exports['progressbar']:Progress({
+    name = 'versa_lib_' .. math.random(1000, 9999),
+    duration = duration,
+    label = label,
+    useWhileDead = states.useWhileDead or false,
+    canCancel = states.canCancel or false,
+    controlDisables = {
+      disableMovement = states.disableMovement,
+      disableCarMovement = states.disableDriving,
+      disableMouse = states.disableMouseMovement,
+      disableCombat = states.disableCombat,
+    },
+    animation = animation,
+    prop = prop or nil,
+    propTwo = propTwo or nil,
+  }, function(cancelled)
+    return (not cancelled)
+  end)
+end
+
+return progressBar
