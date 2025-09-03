ed912e6bc7aa00c4a088a6b091798f05d2c8c57c
Aidan <44532573+ohaidan@users.noreply.github.com>
2025-09-03 16:05:03 +0100

    chore(framework): move example

--- runtime/chunks/server.lua
+++ runtime/chunks/server.lua
@@ -1,7 +1,1 @@
 Chunks = {}
-
-local framework = require '@versa_lib.modules.framework.server'
-RegisterCommand('data', function(source)
-  local player = framework.getPlayer(source)
-  print(json.encode(player, { indent = true }))
-end)
