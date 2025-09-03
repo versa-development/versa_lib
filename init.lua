4ed2417198ca9681a9353675bc40d482b8ee3157
Aidan <44532573+ohaidan@users.noreply.github.com>
2025-09-03 21:28:30 +0100

    feat(init): random seed duplicity on load

--- init.lua
+++ init.lua
@@ -3,6 +3,8 @@ local log = require 'utils.logger'
 
 -- Duplicity checks if the code ran is the server or client. returns true if server
 if IsDuplicityVersion() then
+  math.randomseed()
+
   local resourceVersion = GetResourceMetadata(GetCurrentResourceName(), 'version', 0)   -- TODO: VERSION CHECK!
   log.info('Versa Lib v' .. resourceVersion)
 end
