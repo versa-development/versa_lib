31787a72a49dfc8833fee9297f5f1193f7e67d59
Aidan <44532573+ohaidan@users.noreply.github.com>
2025-09-09 16:06:16 +0100

    chore(progress/bridge/ox): support new prop structure

--- bridge/progress/ox/client.lua
+++ bridge/progress/ox/client.lua
@@ -6,6 +6,19 @@ function progressBar.init(data)
   local prop = data.prop or nil
   local animation = data.animation or nil
   local states = data.states or nil
+
+  -- Versa lib uses "position"/"rotation"; Ox uses "pos"/"rot"
+  if prop ~= nil then
+    if type(prop[1]) == "table" then
+      for i = 1, #prop do
+        prop[i].pos = data.prop[i].position
+        prop[i].rot = data.prop[i].rotation
+      end
+    else
+      prop.pos = data.prop.position
+      prop.rot = data.prop.rotation
+    end
+  end
     
   return lib.progressBar({
     label = label,
