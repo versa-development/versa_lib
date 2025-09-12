a72ecac38016ceff0c5041d357825378cc66ded9
Aidan <44532573+ohaidan@users.noreply.github.com>
2025-09-12 11:47:11 +0100

    notification: ox bridge

---
+++ bridge/notification/ox/client.lua
@@ -0,0 +1,22 @@
+local ox = {}
+
+function ox.notify(data)
+  local title = data.title
+  local message = data.message
+  local duration = data.duration
+  local notificationType = data.type -- info success or error
+  
+  if notificationType == 'info' then
+    notificationType = 'inform'
+  end
+
+  lib.notify({
+    title = title,
+    description = message,
+    duration = duration,
+    showDuration = true,
+    type = notificationType,
+  })
+end
+
+return ox
