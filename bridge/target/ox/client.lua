94873b5c47507a97430c16e15d2ec7bcf16eadd3
Aidan <44532573+ohaidan@users.noreply.github.com>
2025-09-16 16:28:50 +0100

    chore(target): ox/qb bridges

---
+++ bridge/target/ox/client.lua
@@ -0,0 +1,46 @@
+local ox = {}
+local helper = require '@versa_sdk.utils.target'
+
+--- format the target data from the input
+-- @param data table
+-- @return table
+local function formatTargetData(data)
+  if not data.title then error('Missing title property in target data') end
+  if not data.event then error('Missing event property in target data') end
+
+  return {
+    label = data.title,
+    icon = data.icon or 'fa-solid fa-circle',
+    distance = data.distance or 2.5,
+    canInteract = data.canInteract or nil,
+    onSelect = function()
+      local event = data.event.type == 'client' and TriggerEvent or TriggerServerEvent
+      local eventName = data.event.name or error('Missing event name in target data')
+      local parameters = data.event.parameters or {}
+
+      event(eventName, table.unpack(parameters))
+    end
+  }
+end
+
+--- add a target to an entity or multiple entities
+-- @param data table
+-- @return any
+function ox.addEntity(data)
+  local entities = helper.getEntitiesData(data)
+  local targetOptions = formatTargetData(data)
+
+  return exports.ox_target:addLocalEntity(entities, targetOptions)
+end
+
+--- remove a target from an entity or multiple entities
+-- @param data table
+-- @return any
+function ox.removeEntity(data)
+  local entities = helper.getEntitiesData(data)
+
+  return exports.ox_target:removeLocalEntity(entities, { })
+end
+
+-- Send the module object
+return ox
---
+++ bridge/target/qb/client.lua
@@ -0,0 +1,49 @@
+local qb = {}
+local helper = require '@versa_sdk.utils.target'
+
+--- format the target data from the input
+-- @param data table
+-- @return table
+local function formatTargetData(data)
+  if not data.title then error('Missing title property in target data') end
+  if not data.event then error('Missing event property in target data') end
+
+  return {
+    label = data.title,
+    type = 'client',
+    icon = data.icon or 'fa-solid fa-circle',
+    canInteract = data.canInteract or nil,
+    action = function()
+      local event = data.event.type == 'client' and TriggerEvent or TriggerServerEvent
+      local eventName = data.event.name or error('Missing event name in target data')
+      local parameters = data.event.parameters or {}
+
+      event(eventName, table.unpack(parameters))
+    end
+  }
+end
+
+--- add a target to an entity or multiple entities
+-- @param data table
+-- @return any
+function qb.addEntity(data)
+  local entities = helper.getEntitiesData(data)
+  local targetOptions = formatTargetData(data)
+
+  exports['qb-target']:AddTargetEntity(entities, {
+    options = targetOptions,
+    distance = targetOptions.distance or 2.5
+  })
+end
+
+--- remove a target from an entity or multiple entities
+-- @param data table
+-- @return any
+function qb.removeEntity(data)
+  local entities = helper.getEntitiesData(data)
+
+  return exports['qb-target']:RemoveTargetEntity(entities, { })
+end
+
+-- Send the module object
+return qb
