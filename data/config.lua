3b950d17560e8c1fa77c84a36b330dd0da08a522
Aidan <44532573+ohaidan@users.noreply.github.com>
2025-09-16 16:28:59 +0100

    style(config): comments of more bridges

--- data/config.lua
+++ data/config.lua
@@ -3,7 +3,7 @@ return {
 
   Framework = 'qbox', -- esx, qb, qbox, ox
   Target = 'ox', -- ox, qb
-  Inventory = 'ox', -- ox
+  Inventory = 'ox', -- ox, qb
   ProgressBar = 'ox', -- ox, qb
   Notification = 'ox' -- esx, qb, mythic, okok, ox
 }
