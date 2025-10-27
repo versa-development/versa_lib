2efd9c9dac2c077419f2814224f899b5eaeb9d29
Aidan <44532573+ohaidan@users.noreply.github.com>
2025-10-27 01:59:30 +0000

    chore: bump manifest version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,7 +7,7 @@ dependencies {
 
 name 'versa_sdk'
 author 'Versa Development'
-version '0.1'
+version '1.0.0'
 games { 'rdr3', 'gta5' }
 description 'Core SDK for FiveM & RedM — modular systems, framework bridges & dev tools'
 
