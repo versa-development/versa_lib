01815ce0e563c300c7d6dda86c3e37a8331f1d59
Aidan <44532573+ohaidan@users.noreply.github.com>
2025-10-29 13:48:10 +0000

    feat(controls): support multiple controls

--- runtime/controls/client.lua
+++ runtime/controls/client.lua
@@ -3,6 +3,17 @@ local currentlyShown = false
 local types = require 'utils/types'
 local log = require 'utils/logger'
 
+local function getControlString(control)
+    local buttonControl = types.inputControl(control)
+    if not buttonControl then
+        log.warn('Invalid control key provided to showControls: ' .. tostring(control))
+        log.warn('Set new control keys in versa_sdk/data/controls')
+        return false
+    end
+
+    return '~' .. buttonControl .. '~'
+end
+
 RegisterNetEvent('versa_sdk:showControls', function(data)
     if currentlyShown ~= false then
         TriggerEvent('versa_sdk:hideControls')
@@ -19,27 +30,33 @@ RegisterNetEvent('versa_sdk:showControls', function(data)
     CallScaleformMovieMethod(handle, 'CLEAR_ALL')
     CallScaleformMovieMethodWithNumber(handle, 'TOGGLE_MOUSE_BUTTONS', 0)
 
-    -- flip the array as GTA draws them from right to left
+    -- we flip the array as scaleforms draw from right to left
     local reversed = {}
     for i = #data, 1, -1 do
         reversed[#reversed + 1] = data[i]
     end
 
     for i, button in ipairs(reversed) do
-        local buttonControl = types.inputControl(button.control)
-        if not buttonControl then
-            log.warn('Invalid control key provided to showControls: ' .. tostring(button.control))
-            log.warn('Set new control keys in versa_sdk/data/controls')
-            goto continue
+        -- if a single control is provided, convert it to a table for easier processing
+        if type(button.control) ~= 'table' then button.control = { button.control } end
+
+        -- flip the array again for individual button controls as scaleforms draw them right to left
+        local scaleformButtons = {}
+        for s = #button.control, 1, -1 do
+            scaleformButtons[#scaleformButtons + 1] = button.control[s]
         end
 
         BeginScaleformMovieMethod(handle, 'SET_DATA_SLOT')
         ScaleformMovieMethodAddParamInt(i - 1)
-        ScaleformMovieMethodAddParamPlayerNameString('~' .. buttonControl .. '~')
+        
+        -- added support for having multiple controls for a single button
+        for b = 1, #scaleformButtons do
+            local controlString = getControlString(scaleformButtons[b])
+            ScaleformMovieMethodAddParamPlayerNameString(controlString)
+        end
+        
         ScaleformMovieMethodAddParamPlayerNameString(button.text)
         EndScaleformMovieMethod()
-
-        ::continue::
     end
 
     CallScaleformMovieMethod(handle, 'DRAW_INSTRUCTIONAL_BUTTONS')
